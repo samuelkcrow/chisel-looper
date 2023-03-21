5c92f5791a2a2260412104c964808a701f4420cc
Sam Crow <samuelkcrow@gmail.com>
2023-03-20 19:32:30 -0700

    add audio and script files for hello and welcome demo

---
+++ files/mix_raw_16.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ $# -eq 3 ]
+  then
+    sox -m -r 16k -e signed -b 8 -c 1 -v 1 $1 -r 16k -e signed -b 8 -c 1 -v 1 $2 $3
+  else
+    echo "Usage: args 1 & 2 are input audio files, arg 3 is output audio file"
+fi
--- files/play_raw_16.sh
+++ files/play_raw_16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $# -eq 2 ]
   then
-    play -r 8k -e signed -b 16 -c 1 $1 repeat $2
+    play -r 16k -e signed -b 8 -c 1 $1 repeat $2
   else
-    play -r 8k -e signed -b 16 -c 1 $1
+    play -r 16k -e signed -b 8 -c 1 $1
 fi
